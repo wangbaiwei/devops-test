f51d72f6aa595a7fac07fe54ea231137cca1124b
coding <1753749110@qq.com>
2024-11-16 08:44:05 +0800

    devops test

---
+++ src/main/java/com/example/devops_test/DevopsTestApplication.java
@@ -0,0 +1,13 @@
+package com.example.devops_test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevopsTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevopsTestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/devops_test/DevopsTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.devops_test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevopsTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
